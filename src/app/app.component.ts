e2ddfba3c65adb8a2da59b4297bd3e57e5bdaa78
daaren-urthling <daaren.urthling@hotmail.com>
2018-02-07 23:20:06 +0100

    inclusi node fs e parser XML

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component, ChangeDetectorRef } from '@angular/core';
 
-
 declare var dialog: any;
+declare var fs: any;
+declare var parseString: any;
 
 @Component({
   selector: 'app-root',
@@ -12,8 +13,20 @@ export class AppComponent {
   title = 'app';
   fileName:string = "";
   message: string = "";
+  targets: string[];
 
   constructor(private chd: ChangeDetectorRef) {}
+
+  ngOnInit() {
+    fs.readdir('./dist/assets/targets', (err, files) => {
+      this.targets = files;
+      this.chd.detectChanges();
+    });
+    var xml = "<root>Hello xml2js!</root>"
+     parseString(xml, function (err, result) {
+      console.dir(result);
+    });
+  }
 
   onOpen() {
         dialog.showOpenDialog({title:'select'}, (filenames) => {
