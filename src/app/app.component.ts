28a19981e34f83e95fd428ad1a8c1adaf143aace
andrea-rinaldi-microarea <andrea.rinaldi@microarea.it>
2018-02-08 12:20:50 +0100

    parse dbinfo, parse csv, unparse xml e save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,12 @@ import { Component, ChangeDetectorRef } from '@angular/core';
 
 declare var dialog: any;
 declare var fs: any;
-declare var parseString: any;
+declare var xml2js: any;
+declare var path: any;
+declare var xml: any;
+declare var csvParse: any;
+
+const TARGET_REPO = './dist/assets/targets';
 
 @Component({
   selector: 'app-root',
@@ -14,17 +19,16 @@ export class AppComponent {
   fileName:string = "";
   message: string = "";
   targets: string[];
+  result:string[][];
+  input: string;
+  folderName: string = "";
 
   constructor(private chd: ChangeDetectorRef) {}
 
   ngOnInit() {
-    fs.readdir('./dist/assets/targets', (err, files) => {
+    fs.readdir(TARGET_REPO, (err, files) => {
       this.targets = files;
       this.chd.detectChanges();
-    });
-    var xml = "<root>Hello xml2js!</root>"
-     parseString(xml, function (err, result) {
-      console.dir(result);
     });
   }
 
@@ -36,7 +40,73 @@ export class AppComponent {
     });
   }
 
+  onBrowse() {
+        dialog.showOpenDialog({title:'output folder', properties: ["openDirectory"] }, (folders) => {
+      console.log(folders);
+      this.folderName = folders[0];
+      this.chd.detectChanges();
+    });
+  }
+
+  onLoad() {
+    fs.readFile(this.fileName, 'latin1', (err, data) => {
+      if (err) {
+        console.log("Read failed: " + err);
+      }
+      var s1 = JSON.stringify(data);
+      var s2 = JSON.parse(s1);
+      this.input = String(data);
+      csvParse(this.input, {delimiter: ';'}, (err, output) => {
+        console.log(output);
+        this.result= output;
+      });
+    });
+    
+  }
+
+  onTargetSelected(target: string) {
+    fs.readFile(path.join(TARGET_REPO, target), (err, data) => {
+      if (err) {
+        console.log("Read failed: " + err);
+      }
+      this.input = String(data);
+      var parser = new xml2js.Parser();
+      parser.parseString(data, (err, result) => {
+        console.dir(result);
+      });
+    
+    });
+  }
+
   onSave() {
-    this.message = "saved";
+    var output = {
+      DataTables : [
+      ]
+    }
+    for (let idx in this.result) {
+      if (idx == "0")
+        continue;
+      let rec = this.result[idx];
+      output.DataTables.push({MA_Items: { _attr: { 
+        Item : rec[0],
+        Description: rec[5],
+        InternalNote: rec[6],
+        ShortDescription: rec[7],
+        BasePrice: rec[9].replace(',','.'),
+        BaseUoM: rec[12]
+      }}});
+    }
+    var strOut = xml(output, {declaration: { standalone: 'yes', encoding: 'UTF-16' }, indent: true});
+    console.log(strOut);
+    
+    fs.writeFile(path.join(this.folderName, 'MA_Items.xml'), strOut, function (err) {
+      if (err) {
+        console.log("Write failed: " + err);
+        return;
+      }
+  
+      console.log("Write completed.");
+    });
+    
   }
 }
